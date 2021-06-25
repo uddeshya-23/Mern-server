cdcd129ea5b8d0262fc5189f0c945adf6087d589
UDDESHYA KUMAR <uddeshya350q@gmail.com>
2021-06-25 21:47:46 +0530

    fix of name

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
     
     const pubsub = new PubSub();
     
-    const PORT = process.env.port || 5000;
+    const PORT = process.env.PORT || 5000;
    
     if (process.env.NODE_ENV === 'production'){
       app.use(express.static('client/build'));
