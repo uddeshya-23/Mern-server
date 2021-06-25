56eb4d718bdc69ca930cc1ff2a2a590d94f2041f
UDDESHYA KUMAR <uddeshya350q@gmail.com>
2021-06-25 21:37:45 +0530

    init

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 
     const { ApolloServer, PubSub } = require('apollo-server');
     const mongoose = require('mongoose');
-    
+    const path= require('path')
     const typeDefs = require('./graphql/typeDefs');
     const resolvers = require('./graphql/resolvers');
     const { MONGODB } = require('./config.js');
     
     const pubsub = new PubSub();
     
-    const PORT = process.env.PORT || 5000;
-    
+    const PORT = process.env.port || 5000;
+   
+    if (process.env.NODE_ENV === 'production'){
+      app.use(express.static('client/build'));
+      app.get('*',(req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client' , 'index.html'));
+
+      });
+    }
     const server = new ApolloServer({
       typeDefs,
       resolvers,
